245abb7599d5848276263e71dc0389c3eb780ee2
humayun.rashid@dreambroker.fi <humayun.rashid@dreambroker.fi>
2020-09-29 20:42:56 +0300

    updated

---
+++ task-5.py
@@ -0,0 +1,16 @@
+"""
+Task 5.
+Write a program that queries the user for a string containing a DNA sequence, and proceeds
+to output the proportions of four nucleotide bases (A, C, G and T) as percentage values. You
+can assume that the user enters the sequence in lower case. If the user enters an empty string,
+display an error message.
+Example run:
+Enter sequence: aaaagctgcggta
+Proportion of A: 38 percent
+Proportion of T: 15 percent
+Proportion of C: 15 percent
+Proportion of G: 30 percent
+Display the numbers either in Integers (and observe that the example has an inevitable rounding
+error) or display the numbers with decimals.
+"""
+
---
+++ task-6.py
@@ -0,0 +1,66 @@
+"""
+Task 6. (Double points!)
+Write a program that queries a number. The problem is that you don’t know what kind of
+number it is.
+User might enter any of the following:
+123 # a valid Integer
+156.23 # a valid float
+5.22.3 # invalid float (too many points)
+abcd # not a number
+Your task is to:
+1. Check if the value is a valid number
+2. If it is, convert it to the proper type (int or float by using the function)
+3. Print the type
+4. Otherwise print what the problem was
+Example run:
+Give a number: 123
+This is int
+Example run:
+Give a number: 420.69
+This is float
+Example run:
+Give a number: 1.22.34
+Not a number (too many points)
+Example run:
+Give a number: abcd
+Not a number (contains other than numbers)
+Whether or not the string is a number, can be tested with the function my_string.isdecimal().
+The function only returns True, if there are only digits 0-9. This does not count the dots, so you
+might have to replace the dots first with something before using the functions.
+Hint: The string is a number, if it has only decimal characters (0-9) and maximum of one dot.
+Hint: Figure out all the properties first: is there only numbers? how many dots? Then use
+previous lectures information to solve this.
+Note: Exception handling is not needed, as it has not been covered yet
+"""
+
+while True:
+    userNumber= input("Give a number:")
+    checkString = any(map(str.isalpha, userNumber))
+    checkDecimal=any(map(str.isdecimal, userNumber))
+    #checkAllNumber=userNumber.isalnum()
+    checkDigit = any(map(str.isdigit, userNumber))
+    #print(checkString)
+    #print(checkDecimal)
+    #print(checkDigit)
+
+    if "." not in userNumber and checkString==False and (checkDecimal and checkDigit)==True:
+        convertInt = int(userNumber)
+        print("a valid Integer")
+
+    if  checkString==True and checkDecimal==False and checkDigit==False:
+        print("Not a number (contains other than numbers)")
+    
+    if  "." in userNumber and checkString==True and checkDecimal==True and checkDigit==True:
+        print("Not a number (contains other than numbers)")
+
+    if "." in userNumber and checkString==False and (checkDecimal and checkDigit)==True:
+        dotAmount = userNumber.count(".")
+        if dotAmount > 1:
+            print("Not a number (too many points)")
+        if dotAmount == 1:
+            convertFloat = float(userNumber)
+            print("a valid float")
+
+
+
+
